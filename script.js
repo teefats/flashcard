162b0a62d213e47cc5954bc36fd14ce034eca10b
Tunde <tee_fats@yahoo.com>
2020-09-07 21:52:46 +0100

    Added elements

--- script.js
+++ script.js
@@ -30,7 +30,59 @@ const cardsData = [
     },
      {
         question:'Example of Case Variable',
-        answer:"thisIs"
+        answer:"thisIsAVariable"
     },
 
-]
+];
+
+// Create all cards
+function createCards(){
+    cardsData.forEach((data, index) => createCards(data, index))
+}
+
+// Create a single card in dom
+function createCard(data, index) {
+    const card = document.createElement("div")
+    card.classList.add('card');
+
+    if (index ===0) {
+
+card.classList.add('active')
+
+    }
+
+    card.innerHTML= `
+    
+    <div class="inner-card">
+                    <div class="inner-card-front">
+                        <p>
+                            ${data.question}
+                        </p>
+
+                    </div>
+                    <div class="inner-card-back">
+                        <p>${data.answer}</p>
+                    </div>
+                </div>
+    
+    `;
+
+    card.addEventListener('click', () => card.classList.toggle('show-answer'))
+
+    // Add to DOM cards
+    cardsEl.push(card);
+    cardsContainer.appendChild(card);
+
+    updateCurrentText();
+
+
+
+}
+
+// show number of cards
+
+function updateCurrentText() {
+    currentEl.innerText = `${currentActiveCard + 1}/$
+    {cardsEl.length}`
+}
+createCards();
