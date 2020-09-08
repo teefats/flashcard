01290d3f87e391cc6b83cc4c057ba784441b466d
Tunde <tee_fats@yahoo.com>
2020-09-08 19:50:55 +0100

    Final

--- script.js
+++ script.js
@@ -1,88 +1,157 @@
-const cardsContainer =document.querySelector("#cards-container");
-const prevBtn =document.querySelector("#prev");
-const nextBtn =document.querySelector("#next");
-const currentEl =document.querySelector("#current");
-const hideBtn =document.querySelector("#hide");
-const showBtn =document.querySelector("#show");
-const question =document.querySelector("#question");
-const answer =document.querySelector("#answer");
-const addCardBtn =document.querySelector("#add-card");
-const clearBtn =document.querySelector("#clear");
-const addContainer =document.querySelector("#add-container");
+const cardsContainer = document.getElementById('cards-container');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const currentEl = document.getElementById('current');
+const showBtn = document.getElementById('show');
+const hideBtn = document.getElementById('hide');
+const questionEl = document.getElementById('question');
+const answerEl = document.getElementById('answer');
+const addCardBtn = document.getElementById('add-card');
+const clearBtn = document.getElementById('clear');
+const addContainer = document.getElementById('add-container');
 
-
-// keep track o current Card
+// Keep track of current card
 let currentActiveCard = 0;
 
 // Store DOM cards
-const cardEl = [];
+const cardsEl = [];
 
+// Store card data
+const cardsData = getCardsData();
 
-// store Card data
-const cardsData = [
-    {
-        question:'Who killed Waldo?',
-        answer:"Joel Cairo killed Waldo"
-    },
-    {
-        question:'Who is a variable?',
-        answer:"Container for a piece of data"
-    },
-     {
-        question:'Example of Case Variable',
-        answer:"thisIsAVariable"
-    },
-
-];
+// const cardsData = [
+//   {
+//     question: 'What must a variable begin with?',
+//     answer: 'A letter, $ or _'
+//   },
+//   {
+//     question: 'What is a variable?',
+//     answer: 'Container for a piece of data'
+//   },
+//   {
+//     question: 'Example of Case Sensitive Variable',
+//     answer: 'thisIsAVariable'
+//   }
+// ];
 
 // Create all cards
-function createCards(){
-    cardsData.forEach((data, index) => createCards(data, index))
+function createCards() {
+  cardsData.forEach((data, index) => createCard(data, index));
 }
 
-// Create a single card in dom
+// Create a single card in DOM
 function createCard(data, index) {
-    const card = document.createElement("div")
-    card.classList.add('card');
+  const card = document.createElement('div');
+  card.classList.add('card');
 
-    if (index ===0) {
+  if (index === 0) {
+    card.classList.add('active');
+  }
 
-card.classList.add('active')
+  card.innerHTML = `
+  <div class="inner-card">
+  <div class="inner-card-front">
+    <p>
+      ${data.question}
+    </p>
+  </div>
+  <div class="inner-card-back">
+    <p>
+      ${data.answer}
+    </p>
+  </div>
+</div>
+  `;
 
-    }
+  card.addEventListener('click', () => card.classList.toggle('show-answer'));
 
-    card.innerHTML= `
-    
-    <div class="inner-card">
-                    <div class="inner-card-front">
-                        <p>
-                            ${data.question}
-                        </p>
+  // Add to DOM cards
+  cardsEl.push(card);
 
-                    </div>
-                    <div class="inner-card-back">
-                        <p>${data.answer}</p>
-                    </div>
-                </div>
-    
-    `;
+  cardsContainer.appendChild(card);
 
-    card.addEventListener('click', () => card.classList.toggle('show-answer'))
-
-    // Add to DOM cards
-    cardsEl.push(card);
-    cardsContainer.appendChild(card);
-
-    updateCurrentText();
-
-
-
+  updateCurrentText();
 }
 
-// show number of cards
-
+// Show number of cards
 function updateCurrentText() {
-    currentEl.innerText = `${currentActiveCard + 1}/$
-    {cardsEl.length}`
+  currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
 }
+
+// Get cards from local storage
+function getCardsData() {
+  const cards = JSON.parse(localStorage.getItem('cards'));
+  return cards === null ? [] : cards;
+}
+
+// Add card to local storage
+function setCardsData(cards) {
+  localStorage.setItem('cards', JSON.stringify(cards));
+  window.location.reload();
+}
+
 createCards();
+
+// Event listeners
+
+// Next button
+nextBtn.addEventListener('click', () => {
+  cardsEl[currentActiveCard].className = 'card left';
+
+  currentActiveCard = currentActiveCard + 1;
+
+  if (currentActiveCard > cardsEl.length - 1) {
+    currentActiveCard = cardsEl.length - 1;
+  }
+
+  cardsEl[currentActiveCard].className = 'card active';
+
+  updateCurrentText();
+});
+
+// Prev button
+prevBtn.addEventListener('click', () => {
+  cardsEl[currentActiveCard].className = 'card right';
+
+  currentActiveCard = currentActiveCard - 1;
+
+  if (currentActiveCard < 0) {
+    currentActiveCard = 0;
+  }
+
+  cardsEl[currentActiveCard].className = 'card active';
+
+  updateCurrentText();
+});
+
+// Show add container
+showBtn.addEventListener('click', () => addContainer.classList.add('show'));
+// Hide add container
+hideBtn.addEventListener('click', () => addContainer.classList.remove('show'));
+
+// Add new card
+addCardBtn.addEventListener('click', () => {
+  const question = questionEl.value;
+  const answer = answerEl.value;
+
+  if (question.trim() && answer.trim()) {
+    const newCard = { question, answer };
+
+    createCard(newCard);
+
+    questionEl.value = '';
+    answerEl.value = '';
+
+    addContainer.classList.remove('show');
+
+    cardsData.push(newCard);
+    setCardsData(cardsData);
+  }
+});
+
+// Clear cards button
+clearBtn.addEventListener('click', () => {
+  localStorage.clear();
+  cardsContainer.innerHTML = '';
+  window.location.reload();
+});
